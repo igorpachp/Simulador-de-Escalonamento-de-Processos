2019b64782d581d000849f6d33eba52246d61c9d
igorpachp <igorpacheco9@gmail.com>
2023-11-26 14:11:45 -0300

    testando as funções de fila

---
+++ test/queuetest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "queue.h"
+
+int main() {
+    Queue* Q = newQueue();
+    QueueItem* item = NULL;
+
+    push(Q, 1);
+    push(Q, 2);
+    push(Q, 3);
+    printf("Queue has size: %d, head id is: %d, tail id is: %d\n", Q->size, Q->head->id, Q->tail->id);
+
+    item = pop(Q);
+    printf("popped item id is: %d\n", item->id);
+    printf("Queue has size: %d, head id is: %d, tail id is: %d\n", Q->size, Q->head->id, Q->tail->id);
+
+    freeQueueItem(item);
+    if (item == NULL) puts("removido");
+
+    item = pop(Q);
+    printf("popped item id is: %d\n", item->id);
+    printf("Queue has size: %d, head id is: %d, tail id is: %d\n", Q->size, Q->head->id, Q->tail->id);
+    freeQueueItem(item);
+
+    freeQueue(Q);
+}
