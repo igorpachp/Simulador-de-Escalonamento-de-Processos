73ec7506a807b0a97e429360ca45b605278f1886
igorpachp <igorpacheco9@gmail.com>
2023-11-27 14:27:25 -0300

    Testando alterações

--- test/queuetest.c
+++ test/queuetest.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../util.h"
+#include "../process.h"
 #include "../queue.h"
 
 int main() {
+    Process* p[10];
     Queue* Q = newQueue();
-    QueueItem* item = NULL;
+    QueueItem* QI = NULL;
 
-    push(Q, 1);
-    push(Q, 2);
-    push(Q, 3);
-    printf("Queue has size: %d, head id is: %d, tail id is: %d\n", Q->size, Q->head->id, Q->tail->id);
+    for (int i = 0; i < 10; i++) {
+        p[i] = (Process*) malloc(sizeof(Process));
+        p[i]->PID = i;
+        p[i]->PPID = p[i]->PID-1;
+        p[i]->status = READY;
+        p[i]->initialTime = 0;
+        p[i]->currentTime = 5;
+        p[i]->completionTime = 10;
+        printProcess(p[i]);
+    }
 
-    item = pop(Q);
-    printf("popped item id is: %d\n", item->id);
-    printf("Queue has size: %d, head id is: %d, tail id is: %d\n", Q->size, Q->head->id, Q->tail->id);
+    // QueueItem* QI = newQueueItem(p[2]);
 
-    freeQueueItem(item);
-    if (item == NULL) puts("removido");
+    for (int i = 0; i < 10; i++) {
+        push(Q, p[i]);
+    }
+    printf("Queue has size: %d, id for process in head is: %d, id for process in tail is: %d\n", Q->size, Q->head->process->PID, Q->tail->process->PID);
 
-    item = pop(Q);
-    printf("popped item id is: %d\n", item->id);
-    printf("Queue has size: %d, head id is: %d, tail id is: %d\n", Q->size, Q->head->id, Q->tail->id);
-    freeQueueItem(item);
+    QI = pop(Q);
+    printf("popped item id is: %d\n", QI->process->PID);
+    printf("Queue has size: %d, id for process in head is: %d, id for process in tail is: %d\n", Q->size, Q->head->process->PID, Q->tail->process->PID);
+    freeQueueItem(QI);
+
+    QI = pop(Q);
+    printf("popped item id is: %d\n", QI->process->PID);
+    printf("Queue has size: %d, id for process in head is: %d, id for process in tail is: %d\n", Q->size, Q->head->process->PID, Q->tail->process->PID);
+    freeQueueItem(QI);
 
     freeQueue(Q);
 }
