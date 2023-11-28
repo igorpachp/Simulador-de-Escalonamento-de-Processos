b2c0d02bc05abfa2b3aa0eb8daf3e8e6c5251321
igorpachp <igorpacheco9@gmail.com>
2023-11-28 19:41:37 -0300

    Ajustando os valores das constantes

--- util.h
+++ util.h
@@ -2,11 +2,11 @@
 #define __UTIL_H__ "util_lib"
 
 #define QUANTUM         2   // tamanho do time slice para processos em execução
-#define MAX_PROCESS     5   // número de processos na tabela
-#define MAX_ARRIVAL     8  // maior instante de tempo permitido para entrada de um novo processo
-#define MAX_BURST_TIME  10  // limite de tempo de execução necessário para um processo
+#define MAX_PROCESS     3   // número de processos na tabela
+#define MAX_ARRIVAL     6   // maior instante de tempo permitido para entrada de um novo processo
+#define MAX_BURST_TIME  6   // limite de tempo de execução necessário para um processo
 
-#define IO_LIMIT        5   // limite de vezes que um processo pode requisitar dispositivos de IO
+#define IO_LIMIT        2   // limite de vezes que um processo pode requisitar dispositivos de IO
 #define IO_OPTIONS      3   // número de diferentes tipos de dispositivos IO
 #define DISK_TIME       2   // tempo de IO em disco
 #define TAPE_TIME       4   // tempo de IO em fita
